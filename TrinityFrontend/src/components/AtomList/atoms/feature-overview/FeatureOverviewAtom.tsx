41f74929f05d5ab680562a7a3d51ef77d8ca8155
theHdd4 <harshadip.das@quantmatrix.ai>
2025-07-06 01:58:24 +0530

    style: add bottom margin for feature overview

--- TrinityFrontend/src/components/AtomList/atoms/feature-overview/FeatureOverviewAtom.tsx
+++ TrinityFrontend/src/components/AtomList/atoms/feature-overview/FeatureOverviewAtom.tsx
@@ -12,7 +12,7 @@ const FeatureOverviewAtom: React.FC<Props> = ({ atomId }) => {
   const settings: SettingsType = (atom?.settings as SettingsType) || { ...DEFAULT_FEATURE_OVERVIEW_SETTINGS };
 
   return (
-    <div className="w-full h-full bg-white rounded-lg overflow-hidden flex flex-col">
+    <div className="w-full h-full bg-white rounded-lg overflow-hidden flex flex-col mb-2">
       <FeatureOverviewCanvas
         settings={settings}
         onUpdateSettings={s => updateSettings(atomId, s)}
