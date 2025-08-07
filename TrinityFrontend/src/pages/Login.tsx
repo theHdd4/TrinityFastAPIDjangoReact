c3ba44f3a8c37435667b77327e292a5448a88da0
theHdd4 <harshadip.das@quantmatrix.ai>
2025-08-07 20:16:42 +0530

    Allow org-wide project visibility and update demo credentials

--- TrinityFrontend/src/pages/Login.tsx
+++ TrinityFrontend/src/pages/Login.tsx
@@ -144,7 +144,7 @@ const Login = () => {
 
             <div className="text-center">
               <p className="text-black text-xs font-mono">
-                Demo Credentials: neo / neo_the_one
+                Demo Credentials: neo@quantmatrix.ai / neo_the_only_one
               </p>
             </div>
           </CardContent>
