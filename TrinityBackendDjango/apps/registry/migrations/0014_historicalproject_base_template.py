c43a16886bda7a04bd985486026caa96d5df8c0d
theHdd4 <harshadip.das@quantmatrix.ai>
2025-09-01 12:22:10 +0530

    Add migration for historical project base template

--- TrinityBackendDjango/apps/registry/migrations/0014_historicalproject_base_template.py
+++ TrinityBackendDjango/apps/registry/migrations/0014_historicalproject_base_template.py
@@ -5,19 +5,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("registry", "0012_template"),
+        ("registry", "0013_project_base_template"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name="project",
+            model_name="historicalproject",
             name="base_template",
             field=models.ForeignKey(
                 to="registry.template",
                 null=True,
                 blank=True,
-                on_delete=django.db.models.deletion.SET_NULL,
-                related_name="projects",
+                on_delete=django.db.models.deletion.DO_NOTHING,
+                related_name="+",
+                db_constraint=False,
                 help_text="Template this project was created from, if any.",
             ),
         ),
