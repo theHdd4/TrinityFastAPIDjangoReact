3f08009539115f43e69eed5f5d5ba6a91f626547
theHdd4 <harshadip.das@quantmatrix.ai>
2025-09-12 17:15:29 +0530

    feat: add project deletion confirmation and soft delete

--- TrinityBackendDjango/apps/registry/models.py
+++ TrinityBackendDjango/apps/registry/models.py
@@ -51,6 +51,7 @@ class Project(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
     history = HistoricalRecords()
 
     class Meta:
