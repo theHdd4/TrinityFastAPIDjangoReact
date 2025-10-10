f26235f46dc63ac48bc07cd33d2eb199d461c965
theHdd4 <harshadip.das@quantmatrix.ai>
2025-10-10 11:17:46 +0530

    Update Mongo references to trinity_db

--- TrinityBackendFastAPI/app/core/utils.py
+++ TrinityBackendFastAPI/app/core/utils.py
@@ -27,7 +27,7 @@ except ModuleNotFoundError:  # pragma: no cover
     asyncpg = None
 
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "trinity_prod")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "trinity_db")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "trinity_user")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "trinity_pass")
 
