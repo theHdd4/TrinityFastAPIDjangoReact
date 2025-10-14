cf0db20082d65d0e93679d75f7fd3081b6b4b452
QMmahek <mahek.kala@quantmatrix.com>
2025-10-14 17:24:01 +0530

    update in signup

--- TrinityBackendDjango/apps/signups/urls.py
+++ TrinityBackendDjango/apps/signups/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import SignupListViewSet
 
 router = DefaultRouter()
-router.register(r"signups", SignupListViewSet, basename="signup")
+# Use empty string since the prefix is already in config/urls.py
+router.register(r"", SignupListViewSet, basename="signup")
 
 urlpatterns = [
     path("", include(router.urls)),
