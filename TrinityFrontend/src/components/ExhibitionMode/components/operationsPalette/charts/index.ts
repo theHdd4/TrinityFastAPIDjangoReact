9121f7794bbbf1a8a1fe57b3c72aa19145f28ffb
theHdd4 <harshadip.das@quantmatrix.ai>
2025-10-29 11:32:38 +0530

    Remove exhibition chart operations implementation

--- TrinityFrontend/src/components/ExhibitionMode/components/operationsPalette/charts/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './ChartPanel';
-export * from './ChartDataEditor';
-export * from './SlideChart';
-export * from './SlideChartObject';
-export * from './constants';
-export * from './types';
-export * from './slideObject';
